7eae408cbfde2a82069bccaa193f8e796d22ae9a
Bruno Vieira Mendes <87031416+BrunoVieiraMendes@users.noreply.github.com>
2022-11-11 23:14:47 -0300

    Export img figma clean
    * button entrar.

---
+++ app/src/main/java/com/example/apptocomfome/selecaoIngredientes.java
@@ -0,0 +1,14 @@
+package com.example.apptocomfome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class selecaoIngredientes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selecao_ingredientes);
+    }
+}
